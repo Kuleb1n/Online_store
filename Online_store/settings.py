2ea1e14e504135094fbf2d98246e0963509a0e73
Kuleb1n <v_vpyt@mail.ru>
2022-12-11 19:05:09 +0300

    adding applications (products and users) to INSTALLED_APPS

--- Online_store/settings.py
+++ Online_store/settings.py
@@ -40,6 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'products.apps.ProductsConfig',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
