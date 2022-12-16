39b6d1063be1cc370018f4a6e3f630253727bc35
Kuleb1n <v_vpyt@mail.ru>
2022-12-16 23:01:24 +0300

    class User(AbstractUser)

--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField(_("Email address"), unique=True)
+    user_photo = models.ImageField('User photo', upload_to='users/%Y/%m/%d/', blank=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
