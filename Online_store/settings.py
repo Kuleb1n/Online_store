5a3bca4a6cc07e9f1e928e130d1b3fde9115a92a
Kuleb1n <v_vpyt@mail.ru>
2022-12-17 23:21:25 +0300

    MEDIA_URL MEDIA_ROOT

--- Online_store/settings.py
+++ Online_store/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 
 import environ
@@ -134,3 +134,6 @@ STATICFILES_DIRS = [
 ]
 
 AUTH_USER_MODEL = 'users.User'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
