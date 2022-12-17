28e52f2f7b72c9dca37da6f6702e223ad554d690
Kuleb1n <v_vpyt@mail.ru>
2022-12-17 23:22:23 +0300

    model User

--- users/models.py
+++ users/models.py
@@ -9,3 +9,10 @@ class User(AbstractUser):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
