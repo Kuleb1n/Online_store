ee414d88b3feb610e55d56830286097bf19998c4
Kuleb1n <v_vpyt@mail.ru>
2022-12-17 23:23:15 +0300

    admin.site.register(User, UserAdmin)

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from users.models import *
 
-# Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'email', 'first_name', 'last_name', 'user_photo', 'date_joined',
+                    'is_staff']
+    list_display_links = ['username', 'email']
+    search_fields = ['username', 'email']
+    list_filter = ['date_joined', 'is_staff']
+    fields = ('username', 'email', 'first_name', 'last_name', 'user_photo',
+              'is_staff', 'is_active', 'is_superuser')
+
+
+admin.site.register(User, UserAdmin)
