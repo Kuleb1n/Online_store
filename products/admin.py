e55e88b1e8620c13e66e5b93c41482e63aef2448
Kuleb1n <v_vpyt@mail.ru>
2022-12-17 23:23:26 +0300

    admin.site.register(ProductCategory, ProductCategoryAdmin) admin.site.register(Product, ProductAdmin)

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from products.models import *
 
-# Register your models here.
+
+class ProductCategoryAdmin(admin.ModelAdmin):
+    list_display = ['title', 'description', 'slug']
+    list_display_links = ['title']
+    prepopulated_fields = {"slug": ("title",)}
+    fields = ('title', 'slug', 'description')
+    search_fields = ['title', 'description']
+    list_filter = ['title']
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'image', 'description', 'short_description', 'price', 'quantity', 'category', 'slug']
+    list_display_links = ['title', 'category']
+    list_filter = ['title', 'price', 'category']
+    search_fields = ['title', 'description', 'short_description']
+    fields = ('title', 'slug', 'image', 'description', 'short_description', 'price', 'quantity', 'category')
+    prepopulated_fields = {"slug": ("title",)}
+
+
+admin.site.register(ProductCategory, ProductCategoryAdmin)
+admin.site.register(Product, ProductAdmin)
