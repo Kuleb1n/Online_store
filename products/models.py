c815e139a1f28a19b649b333b6fd79a2a3bdcedc
Kuleb1n <v_vpyt@mail.ru>
2022-12-17 23:22:51 +0300

    models ProductCategory | Product

--- products/models.py
+++ products/models.py
@@ -2,9 +2,16 @@ from django.db import models
 
 
 class ProductCategory(models.Model):
-    title = models.CharField('Title', max_length=64, unique=True)
+    title = models.CharField('Title', max_length=150, unique=True)
     description = models.TextField('Description', blank=True)
-    slug = models.SlugField('URL', max_length=264, unique=True, db_index=True)
+    slug = models.SlugField('URL', unique=True, db_index=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Product Category'
+        verbose_name_plural = 'Product Categories'
 
 
 class Product(models.Model):
@@ -14,5 +21,12 @@ class Product(models.Model):
     short_description = models.CharField('Short_description', max_length=64, blank=True)
     price = models.DecimalField('Price', max_digits=8, decimal_places=2, default=0)
     quantity = models.PositiveIntegerField('Quantity', default=0)
-    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
-    slug = models.SlugField('URL', max_length=264, unique=True, db_index=True)
+    category = models.ForeignKey('ProductCategory', on_delete=models.CASCADE)
+    slug = models.SlugField('URL', unique=True, db_index=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
