8d6e9c1be64435aa8cd836e30e1a373b63f4b0fc
Kuleb1n <v_vpyt@mail.ru>
2022-12-18 22:50:47 +0300

    path('account/', include('users.urls'))

--- Online_store/urls.py
+++ Online_store/urls.py
@@ -22,6 +22,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('products.urls')),
+    path('account/', include('users.urls'))
 ]
 
 if settings.DEBUG:
