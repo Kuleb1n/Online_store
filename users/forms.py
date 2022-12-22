f279bbba4db18c41f997552f5d7995306dc98fee
Kuleb1n <v_vpyt@mail.ru>
2022-12-22 23:57:24 +0300

    adding class UserRegistrationForm(UserCreationForm)

--- users/forms.py
+++ users/forms.py
@@ -1,10 +1,10 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from .models import User
 
 
 class UserLoginForm(AuthenticationForm):
-    email = forms.EmailField(widget=forms.EmailInput(attrs={
+    username = forms.CharField(widget=forms.TextInput(attrs={
         'placeholder': 'Enter your email',
     }))
     password = forms.CharField(widget=forms.TextInput(attrs={
@@ -14,6 +14,24 @@ class UserLoginForm(AuthenticationForm):
     class Meta:
         model = User
         fields = ['email', 'password']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control py-4'
+
+
+class UserRegistrationForm(UserCreationForm):
+    first_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Enter a name'}))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Enter your last name'}))
+    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Enter the user name'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder': 'Enter your email address'}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Enter the password'}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Repeat the password'}))
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
