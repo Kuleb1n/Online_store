cca024959e77f58ad2091f3f27dc1b650a703327
Kuleb1n <v_vpyt@mail.ru>
2022-12-23 23:10:19 +0300

    path('profile/', profile, name='profile')

--- users/urls.py
+++ users/urls.py
@@ -4,4 +4,5 @@ from users.views import *
 urlpatterns = [
     path('login/', login, name='login'),
     path('register/', register, name='register'),
+    path('profile/', profile, name='profile'),
 ]
