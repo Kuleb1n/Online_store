1c03e467cbe85c57a0b158d16bf1ee30d34083d8
Kuleb1n <v_vpyt@mail.ru>
2022-12-24 23:25:38 +0300

    adding 'logout/'

--- users/urls.py
+++ users/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('login/', login, name='login'),
     path('register/', register, name='register'),
     path('profile/', profile, name='profile'),
+    path('logout/', logout, name='logout'),
 ]
