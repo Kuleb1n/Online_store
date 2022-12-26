926bba76b49659424fc85c49296a2a35866551ef
Kuleb1n <v_vpyt@mail.ru>
2022-12-26 22:58:52 +0300

    routes: 'basket-add/<int:product_id>/' ; 'basket-delete/<int:pk>/'

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import *
+from products.views import *
 
 urlpatterns = [
     path('', index, name='index'),
     path('products/', products, name='products'),
+    path('basket-add/<int:product_id>/', basket_add, name='basket_add'),
+    path('basket-delete/<int:pk>/', basket_delete, name='basket_delete'),
 ]
