fc13bf5737b7a50470e5d51cb06857f781d2bd51
Kuleb1n <v_vpyt@mail.ru>
2022-12-26 22:57:41 +0300

    basket_add | basket_delete

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from .models import *
+from products.models import *
 
 
 def index(request):
@@ -14,3 +15,24 @@ def products(request):
         'Products': Products,
     }
     return render(request, 'products/products.html', context)
+
+
+def basket_add(request, product_id):
+    current_page = request.META.get('HTTP_REFERER')
+    product = Product.objects.get(id=product_id)
+    baskets = Basket.objects.filter(user=request.user, product=product)
+
+    if not baskets.exists():
+        Basket.objects.create(user=request.user, product=product, quantity=1)
+        return HttpResponseRedirect(current_page)
+    else:
+        basket = baskets.first()
+        basket.quantity += 1
+        basket.save()
+        return HttpResponseRedirect(current_page)
+
+
+def basket_delete(request, pk):
+    basket = Basket.objects.get(id=pk)
+    basket.delete()
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
