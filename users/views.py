1ea0550677e0f5929debdb0c724cd99b2937b61a
Kuleb1n <v_vpyt@mail.ru>
2022-12-28 22:35:43 +0300

    @login_required

--- users/views.py
+++ users/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, HttpResponseRedirect
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 from django.contrib import auth, messages
@@ -38,6 +39,7 @@ def register(request):
     return render(request, 'users/register.html', context)
 
 
+@login_required
 def profile(request):
     user = request.user
     if request.method == 'POST':
