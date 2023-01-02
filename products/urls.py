ea79e8bd604de63850d3c36f60a0fbb342f015b4
Kuleb1n <v_vpyt@mail.ru>
2023-01-02 23:38:49 +0300

    path('page/<int:page>/', products, name='page')

--- products/urls.py
+++ products/urls.py
@@ -4,6 +4,7 @@ from products.views import *
 urlpatterns = [
     path('', index, name='index'),
     path('products/', products, name='products'),
+    path('page/<int:page>/', products, name='page'),
     path('<slug:category_slug>/', products, name='category'),
     path('basket-add/<int:product_id>/', basket_add, name='basket_add'),
     path('basket-delete/<int:pk>/', basket_delete, name='basket_delete'),
