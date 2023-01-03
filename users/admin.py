7e07c9e6dbe92c48dac6f92d03ee742556337237
Kuleb1n <v_vpyt@mail.ru>
2023-01-03 21:50:27 +0300

    setting up the admin panel

--- users/admin.py
+++ users/admin.py
@@ -1,15 +1,28 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
 from users.models import *
+from products.admin import BasketAdminInline
 
 
 class UserAdmin(admin.ModelAdmin):
-    list_display = ['username', 'email', 'first_name', 'last_name', 'user_photo', 'date_joined',
+    list_display = ['username', 'email', 'first_name', 'last_name', 'get_photo', 'date_joined',
                     'is_staff']
     list_display_links = ['username', 'email']
     search_fields = ['username', 'email']
     list_filter = ['date_joined', 'is_staff']
-    fields = ('username', 'email', 'first_name', 'last_name', 'user_photo',
+    fields = ('username', 'email', 'first_name', 'last_name', 'get_photo', 'user_photo',
               'is_staff', 'is_active', 'is_superuser')
+    inlines = (BasketAdminInline,)
+    readonly_fields = ('get_photo',)
+
+    def get_photo(self, object):
+        if object.user_photo:
+            return mark_safe(f"<img src='{object.user_photo.url}' width=100")
+        else:
+            return 'The user is missing a photo'
+
+    get_photo.short_description = "User's installed photo"
 
 
 admin.site.register(User, UserAdmin)
