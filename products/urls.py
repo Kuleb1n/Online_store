4555777b158ccb97ff942aa894c70b278869bbde
Kuleb1n <v_vpyt@mail.ru>
2023-02-12 19:33:25 +0300

    IndexView.as_view() | ProductsListView.as_view()

--- products/urls.py
+++ products/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from products.views import *
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('products/', products, name='products'),
-    path('page/<int:page>/', products, name='page'),
-    path('<slug:category_slug>/', products, name='category'),
+    path('', IndexView.as_view(), name='index'),
+    path('products/', ProductsListView.as_view(), name='products'),
+    path('page/<int:page>/', ProductsListView.as_view(), name='page'),
+    path('<slug:category_slug>/', ProductsListView.as_view(), name='category'),
     path('basket-add/<int:product_id>/', basket_add, name='basket_add'),
     path('basket-delete/<int:pk>/', basket_delete, name='basket_delete'),
 ]
