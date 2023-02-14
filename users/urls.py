249756795ab416e1696a466653e59049e69bfba5
Kuleb1n <v_vpyt@mail.ru>
2023-02-14 22:23:54 +0300

    changing routes

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,10 @@
+from django.contrib.auth.decorators import login_required
 from django.urls import path
-from users.views import *
+from users.views import UserLoginView, UserRegistrationView, UserProfileView, LogoutUserView
 
 urlpatterns = [
-    path('login/', login, name='login'),
-    path('register/', register, name='register'),
-    path('profile/', profile, name='profile'),
-    path('logout/', logout, name='logout'),
+    path('login/', UserLoginView.as_view(), name='login'),
+    path('register/', UserRegistrationView.as_view(), name='register'),
+    path('profile/<int:pk>/', login_required(UserProfileView.as_view()), name='profile'),
+    path('logout/', LogoutUserView.as_view(), name='logout'),
 ]
