fa4d1382d75f3ed130a2cf21eb4de1b83f3f2a0d
Kuleb1n <v_vpyt@mail.ru>
2023-02-14 22:24:33 +0300

    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))

--- products/views.py
+++ products/views.py
@@ -30,18 +30,17 @@ class ProductsListView(ListView):
 
 @login_required
 def basket_add(request, product_id):
-    current_page = request.META.get('HTTP_REFERER')
     product = Product.objects.get(id=product_id)
     baskets = Basket.objects.filter(user=request.user, product=product)
 
     if not baskets.exists():
         Basket.objects.create(user=request.user, product=product, quantity=1)
-        return HttpResponseRedirect(current_page)
     else:
         basket = baskets.first()
         basket.quantity += 1
         basket.save()
-        return HttpResponseRedirect(current_page)
+
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 
 @login_required
