a6d71085c9768c17df5908e64bc30b7779bb26ef
Kuleb1n <v_vpyt@mail.ru>
2023-02-16 21:54:13 +0300

    creating a class EmailConfirmation

--- users/models.py
+++ users/models.py
@@ -6,6 +6,7 @@ from django.db import models
 class User(AbstractUser):
     email = models.EmailField(_("Email address"), unique=True)
     user_photo = models.ImageField('User photo', upload_to='users/%Y/%m/%d/', blank=True)
+    is_the_email_confirmed = models.BooleanField(default=False)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
@@ -16,3 +17,13 @@ class User(AbstractUser):
     class Meta:
         verbose_name = 'User'
         verbose_name_plural = 'Users'
+
+
+class EmailConfirmation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='User')
+    code = models.UUIDField('Code', unique=True)
+    created = models.DateTimeField('Created', auto_now_add=True)
+    expiration = models.DateTimeField()
+
+    def __str__(self):
+        return f'EmailConfirmation object for {self.user.email}'
