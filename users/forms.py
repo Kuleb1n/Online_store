5730ae87bac44712853d5c164f319b62c14dad78
Kuleb1n <v_vpyt@mail.ru>
2023-02-20 21:41:10 +0300

    adding save(self, commit=True)

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,11 @@
+import uuid
+from datetime import timedelta
+
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
-from .models import User
+from django.utils.timezone import now
+
+from .models import User, EmailConfirmation
 
 
 class UserLoginForm(AuthenticationForm):
@@ -32,6 +37,13 @@ class UserRegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
+
+    def save(self, commit=True):
+        user = super(UserRegistrationForm, self).save(commit=True)
+        expiration = now() + timedelta(hours=48)
+        record_data = EmailConfirmation.objects.create(code=uuid.uuid4(), user=user, expiration=expiration)
+        record_data.send_verification_email()
+        return user
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
