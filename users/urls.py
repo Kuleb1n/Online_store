99f39bc130cb646bcb381139cdb49126352ef126
Kuleb1n <v_vpyt@mail.ru>
2023-02-22 23:10:27 +0300

    path 'email-confirmation/<str:email>/<uuid:code>/'

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,11 @@
 from django.contrib.auth.decorators import login_required
 from django.urls import path
-from users.views import UserLoginView, UserRegistrationView, UserProfileView, LogoutUserView
+from users.views import UserLoginView, UserRegistrationView, UserProfileView, LogoutUserView, EmailConfirmationView
 
 urlpatterns = [
     path('login/', UserLoginView.as_view(), name='login'),
     path('register/', UserRegistrationView.as_view(), name='register'),
     path('profile/<int:pk>/', login_required(UserProfileView.as_view()), name='profile'),
     path('logout/', LogoutUserView.as_view(), name='logout'),
+    path('email-confirmation/<str:email>/<uuid:code>/', EmailConfirmationView.as_view(), name='email-confirmation'),
 ]
